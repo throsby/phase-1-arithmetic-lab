804383a60355675a7cd0adfe9c38dd3875a504ef
Throsby Wells <throsbywells@gmail.com>
2022-03-16 14:43:53 -0400

    commit tree 5e4e620f0db49523c0b09198d0ee16296d3f2622

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ let num1 = 31;
 let num2 = 2
 let multiply = num1 * num2
 
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 24
+let num4 = 20
+let mod = num3 % num4
+
+let max = Math.max(3,2,5,4,7,20)
